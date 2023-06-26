b4099c3bd407aab142f8b6643838be438eb3ca4b
yunasim <ynsim@solidit.co.kr>
2023-06-26 23:04:02 +0900

    #😎update: 이터레이터 클래스 활용

--- IteratorPattern/Program.cs
+++ IteratorPattern/Program.cs
@@ -10,6 +10,26 @@ namespace IteratorPattern
     {
         static void Main(string[] args)
         {
+            var collection = new WordsCollection();
+            collection.AddItem("첫번째");
+            collection.AddItem("두번째");
+            collection.AddItem("세번째");
+
+            Console.WriteLine("순방향 순회:");
+
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n역방향 순회:");
+
+            collection.ReverseDirection();
+
+            foreach(var item in collection)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
